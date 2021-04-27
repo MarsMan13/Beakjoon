07d962ed3f9421bf367037954b4c2af0a7cd48a4
MarsMan13 <choigeon0513@gmail.com>
2021-04-27 14:35:21 +0000

    ttt

--- Java_tb/2931.java
+++ Java_tb/2931.java
@@ -23,6 +23,7 @@ class Main{
 				char tempC = tempS.charAt(j-1);
 				if(tempC == 'M'){
 					M_i = i; M_j = j;
+					map[i][j] = new Room(i, j, '+');
 				}
 				else if(tempC == 'Z'){
 					Z_i = i; Z_j = j;
@@ -32,54 +33,69 @@ class Main{
 					map[i][j] = new Room(i, j, tempC);	
 			}
 		}
-		
+	
 		// END OF INIT
 		
 		int pointer_i = M_i; int pointer_j = M_j;
-		int pointer_d = -1;
-		Room pointer_room = null;
 		for(int i = 0; i<4; i++){
-			if(map[pointer_i+ii[i]][pointer_j+jj[i]] != null){
-				pointer_i = pointer_i + ii[i];
-				pointer_j = pointer_j + jj[i];
-				pointer_room = map[pointer_i][pointer_j];	
+			if(map[M_i+ii[i]][M_j+jj[i]] != null){
+				pointer_i = M_i + ii[i];
+				pointer_j = M_j + jj[i];
 			}
 		}
 		
 		// END OF FINDING STARTING ROOM
 		
+		Pipe pointer_pipe = null;
 		{
 			int gap_i = pointer_i - M_i;
 			int gap_j = pointer_j - M_j;
-			
 			Pipe tempPipe = new Pipe(gap_i, gap_j);
-			tempPipe.i = pointer_i;	tempPipe.j = pointer_j;
-			pointer_d = tempPipe.getNext();
+			int nextPipeType = tempPipe.getNext();
+			pointer_pipe = map[M_i][M_j].pipes[nextPipeType];
 		}
 	
-		int counter = 0;
-		Pipe pointer_pipe = pointer_room.pipes[pointer_d];
+		def1(pointer_pipe, 0);
+		System.out.println(answer_i + " " + answer_j + " " + (char)answer_c);
 		
-		while(pointer_pipe.i != Z_i || pointer_pipe.j != Z_j){
-			pointer_pipe = pointer_pipe.getNextPipe();
-			counter++;
-		}
-	
-		System.out.println(counter);
-		
-	}
-	
-	int def1(Pipe pointer_pipe){
-	
-		while(pointer_pipe.i != Z_i || pointer_pipe.j != Z_j){
-			pointer_pipe = pointer_pipe.getNextPipe();
-			
-		}
-		
-		
-		return 0;
 	}
 
+	static int answer_i = 0, answer_j = 0;
+	static char answer_c = 0;
+	
+	static int def1(Pipe pointer_pipe, int flag){
+
+		if(pointer_pipe.i == Z_i && pointer_pipe.j == Z_j)
+			return 1;
+	
+		Pipe next_pipe = pointer_pipe.getNextPipe();
+
+		if(next_pipe == null){
+			if(flag == 1){
+				return 0;
+			}
+			int ret = 0;
+			int target_i = pointer_pipe.getNext_i();
+			int target_j = pointer_pipe.getNext_j();
+			for(int b = 0; b < blocks.length; b++){
+				map[target_i][target_j] = new Room(target_i, target_j, blocks[b]);
+				ret = def1(pointer_pipe, 1);
+				if(ret == 0){
+					map[target_i][target_j] = null;
+				}
+				else if(ret == 1){
+					answer_i = target_i;
+					answer_j = target_j;
+					answer_c = blocks[b];
+					return 1;
+				}
+			}	
+		}
+		return def1(next_pipe, flag);
+	}
+
+	static char[] blocks = {'|', '-', '+', '1', '2', '3', '4'};
+	
 	static int[] ii = {1, -1, 0, 0};
 	static int[] jj = {0, 0, 1, -1};
 	
@@ -89,7 +105,7 @@ class Room{
 
 	int i, j;
 	Pipe[] pipes = new Pipe[5];
-	Pipe from_up, from_down, from_left, from_right;	
+	Pipe from_up = null, from_down = null, from_left = null, from_right = null;	
 	// each id: 1, 2, 3, 4
 	
 	Room(int i, int j, char type){
@@ -122,8 +138,8 @@ class Room{
 				from_up = new Pipe(0, -1);
 				break;
 			case '4':
-				from_up = new Pipe(1, 0);
-				from_down = new Pipe(-1, 0);
+				from_left = new Pipe(1, 0);
+				from_down = new Pipe(0, -1);
 				break;
 		}
 		pipes[1] = from_up; 	pipes[4] = from_right;
@@ -155,6 +171,9 @@ class Pipe{
 	
 	Pipe getNextPipe(){
 		Room nextRoom = getNextRoom();
+		if(nextRoom == null)
+			return null;
+		// else
 		if(ii == 0){
 			if(jj == 1)
 				return nextRoom.from_left;
@@ -167,6 +186,14 @@ class Pipe{
 			return nextRoom.from_down;
 		return null;
 		
+	}
+	
+	int getNext_i(){
+		return i+ii;
+	}
+	
+	int getNext_j(){
+		return j+jj;
 	}
 
 	int getNext(){
