b15adf906c2eb972ad178eb2a2dcad1a2a0481df
MarsMan13 <choigeon0513@gmail.com>
2021-05-05 12:48:53 +0000

    21.05.05.

--- Java_tb/7662.java
+++ Java_tb/7662.java
@@ -16,27 +16,29 @@ class Main{
 		for(int t = 0; t<T; t++){
 			st = new StringTokenizer(bf.readLine());
 			int numCom = Integer.parseInt(st.nextToken());
-			
-			PriorityQueue<Integer> queueForBig = new PriorityQueue<>(Collections.reverseOrder());
-			PriorityQueue<Integer> queueForSml = new PriorityQueue<>();
-			
+		
+			DualPriorityQueue queue = new DualPriorityQueue(1000000);
 			for(int c = 0; c < numCom; c++){
 				st = new StringTokenizer(bf.readLine());
 				String command = st.nextToken();
 				int value = Integer.parseInt(st.nextToken());
 				
 				if(command.equals("I")){
-					queueForBig.offer(value);
-					queueForSml.offer(value);
+					queue.insert(value);
 				}
 				else if(command.equals("D")){
-					if(value == 1){
-						queueForBig.poll();
-					}
-					else if(value == -1){
-						queueForSml.poll();
-					}
+					queue.delete(value);
 				}
+			}
+			if(queue.size < 1){
+				sb.append("EMPTY");
+				sb.append("\n");
+			}
+			else{
+				sb.append(queue.heap_straight[1].n);
+				sb.append(" ");
+				sb.append(queue.heap_reverse[1].n);
+				sb.append("\n");
 			}
 		}
 		
@@ -62,44 +64,43 @@ class DualPriorityQueue{
 		if(size == 0)
 			return;
 		
-		int comparator = 1;
-		int[] heap = heap_straight;
-		if(d == -1){
-			heap = heap_reverse;
-			comparator = -1;
-		}
+		Dipole[] heap = heap_reverse;
+		if(d == 1)
+			heap = heap_straight;
 		Dipole target = heap[1];
-		heap[1] = heap[size--];
-		heap[1].index = 1;
+		heap[1].n = heap[size--].n;
 		heapify(heap, d, 1);
 		
 		target = target.friend;
 		
-		
-		
-		return ret;
+		heap = heap_straight;
+		if(d == 1)
+			heap = heap_reverse;
+		heap[target.index].n = heap[size+1].n;
+		heapify(heap, d * -1, target.index);
 	}
 	
-	public void heapify(int[] heap, int comparator, int index){
+	public void heapify(Dipole[] heap, int comparator, int index){
 		int leftChild = index * 2;
 		while(leftChild <= size && 
-			  (heap[index]*comparator > heap[leftChild]*comparator || 
-			   heap[index]*comparator > heap[leftChild + 1]*comparator)){
+			  (heap[index].n*comparator < heap[leftChild].n*comparator || 
+			   heap[index].n*comparator < heap[leftChild + 1].n*comparator)){
 			int next = leftChild;
 			if(leftChild + 1 <= size){
-				if(heap[leftChild + 1]*comparator < heap[leftChild]*comparator)
+				if(heap[leftChild + 1].n*comparator > heap[leftChild].n*comparator)
 					next = leftChild + 1;
+			}
+			{
+				Dipole temp = heap[index];
+				heap[index] = heap[next];
+				heap[next] = temp;
 			}
 			{
 				int temp = heap[index].index;
 				heap[index].index = heap[next].index;
 				heap[next].index = temp;
 			}
-			{
-				int temp = heap[index];
-				heap[index] = heap[next];
-				heap[next] = temp;
-			}	
+			
 			index = next;
 			leftChild = index * 2;
 		}
@@ -109,21 +110,28 @@ class DualPriorityQueue{
 	public void insert(int value){
 		size++;
 		Dipole dipole1 = new Dipole(value, size);
-		Dipole dipole2 = new Dipole(valua, size);
+		Dipole dipole2 = new Dipole(value, size);
 		Dipole.makeDipole(dipole1, dipole2);
 		heap_straight[size] = dipole1;
 		heap_reverse[size] = dipole2;
 		
 		for(int comparator = -1; comparator<=1; comparator+=2){
-			int[] heap = heap_straght;
-			if(comparator == 1)
+			Dipole[] heap = heap_straight;
+			if(comparator == -1)
 				heap = heap_reverse;
 			int index = size;
 			int parent = size / 2;
-			while(1 <= parent && heap[index]*comparator < heap[parent]*comparator){
-				int temp = heap[index];
-				heap[index] = heap[parent];
-				heap[parent] = temp;
+			while(1 <= parent && heap[index].n*comparator > heap[parent].n*comparator){
+				{
+					Dipole temp = heap[index];
+					heap[index] = heap[parent];
+					heap[parent] = temp;
+				}
+				{
+					int temp = heap[index].index;
+					heap[index].index = heap[parent].index;
+					heap[parent].index = temp;
+				}
 				index = parent;
 				parent = index/2;
 			}	
@@ -155,21 +163,6 @@ class Dipole{
 	}
 	
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
