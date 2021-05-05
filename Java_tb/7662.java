f6a4d0ad174115e7b23fdb325307a91d70ae349c
MarsMan13 <choigeon0513@gmail.com>
2021-05-05 04:23:00 +0000

    21.05.05.

--- Java_tb/7662.java
+++ Java_tb/7662.java
@@ -38,19 +38,6 @@ class Main{
 					}
 				}
 			}
-			List<Integer> resultBig = new ArrayList<>(queueForBig);
-			List<Integer> resultSml = new ArrayList<>(queueForSml);
-			resultBig.retainAll(resultSml);
-			if(resultBig.isEmpty()){
-				sb.append("EMPTY");
-				sb.append("\n");
-				continue;
-			}
-			Collections.sort(resultBig);
-			sb.append(resultBig.get(resultBig.size() - 1));
-			sb.append(" ");
-			sb.append(resultBig.get(0));
-			sb.append("\n");
 		}
 		
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -58,4 +45,131 @@ class Main{
 		bw.flush(); bw.close();
 	}
 }
+
+class DualPriorityQueue{
+
+	int size = 0;
+	Dipole[] heap_straight = null;	//bigest,	comparator == -1
+	Dipole[] heap_reverse = null;	//smallest, comparator == 1
+	
+	DualPriorityQueue(int n){
+		this.heap_straight = new Dipole[n+1];
+		this.heap_reverse = new Dipole[n+1];
+	}
+
+	public void delete(int d){
+
+		if(size == 0)
+			return;
+		
+		int comparator = 1;
+		int[] heap = heap_straight;
+		if(d == -1){
+			heap = heap_reverse;
+			comparator = -1;
+		}
+		Dipole target = heap[1];
+		heap[1] = heap[size--];
+		heap[1].index = 1;
+		heapify(heap, d, 1);
+		
+		target = target.friend;
+		
+		
+		
+		return ret;
+	}
+	
+	public void heapify(int[] heap, int comparator, int index){
+		int leftChild = index * 2;
+		while(leftChild <= size && 
+			  (heap[index]*comparator > heap[leftChild]*comparator || 
+			   heap[index]*comparator > heap[leftChild + 1]*comparator)){
+			int next = leftChild;
+			if(leftChild + 1 <= size){
+				if(heap[leftChild + 1]*comparator < heap[leftChild]*comparator)
+					next = leftChild + 1;
+			}
+			{
+				int temp = heap[index].index;
+				heap[index].index = heap[next].index;
+				heap[next].index = temp;
+			}
+			{
+				int temp = heap[index];
+				heap[index] = heap[next];
+				heap[next] = temp;
+			}	
+			index = next;
+			leftChild = index * 2;
+		}
+	}
+	
+
+	public void insert(int value){
+		size++;
+		Dipole dipole1 = new Dipole(value, size);
+		Dipole dipole2 = new Dipole(valua, size);
+		Dipole.makeDipole(dipole1, dipole2);
+		heap_straight[size] = dipole1;
+		heap_reverse[size] = dipole2;
+		
+		for(int comparator = -1; comparator<=1; comparator+=2){
+			int[] heap = heap_straght;
+			if(comparator == 1)
+				heap = heap_reverse;
+			int index = size;
+			int parent = size / 2;
+			while(1 <= parent && heap[index]*comparator < heap[parent]*comparator){
+				int temp = heap[index];
+				heap[index] = heap[parent];
+				heap[parent] = temp;
+				index = parent;
+				parent = index/2;
+			}	
+		}
+		
+	}
+	
+	
+}
+
+class Dipole{
+	
+	int n = 0;
+	int index = 0;
+	Dipole friend = null;
+	
+	Dipole(int n){
+		this.n = n;
+	}
+	
+	Dipole(int n, int index){
+		this.n = n;
+		this.index = index;
+	}
+	
+	public static void makeDipole(Dipole d1, Dipole d2){
+		d1.friend = d2;
+		d2.friend = d1;
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
