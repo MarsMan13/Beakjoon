179563a7b3dd258b97850bce5483437ddc462470
MarsMan13 <choigeon0513@gmail.com>
2021-05-14 10:53:42 +0000

    21.05.14.

---
+++ Java_tb/15654_2.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.io.*;
+
+
+class Main{
+	
+	static int N, M;
+	static LinkedList<Integer> inputs = null;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		
+		
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		inputs = new LinkedList<>();
+		
+		st = new StringTokenizer(bf.readLine());
+		for(int i = 0; i<N; i++)
+			inputs.add(Integer.parseInt(st.nextToken()));
+	
+		Collections.sort(inputs);
+		
+		def1(new LinkedList<Integer>());
+		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write(sb.toString());
+		bw.flush(); bw.close();
+		
+	}
+	
+	public static void def1(LinkedList<Integer> list){
+		if(list.size() == M){
+			for(int i : list){
+				sb.append(i);
+				sb.append(" ");
+			}
+			sb.append("\n");
+		}
+		int size = inputs.size();
+		for(int i = 0; i<size; i++){
+			int temp = inputs.remove(i);
+			list.add(temp);
+			def1(list);
+			list.remove(list.size() - 1);
+			inputs.add(i, temp);
+		}
+	}
+}
+
