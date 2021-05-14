62feed6d13e2f63bf5ee5b3bbcdaa0384a8fdd17
MarsMan13 <choigeon0513@gmail.com>
2021-05-14 14:11:43 +0000

    21.05.14.

---
+++ Java_tb/1918.java
@@ -0,0 +1,58 @@
+import java.util.*;
+import java.io.*;
+
+
+class Main{
+	
+	static String input;
+	static Stack<Character> stack = new Stack<>();
+	
+	public static void main(String[] args) throws IOException {
+		
+		Scanner sc = new Scanner(System.in);
+		input = sc.nextLine();
+	
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i = 0; i<input.length(); i++){
+			char target = input.charAt(i);
+	
+			switch(target){
+				case '(':
+					stack.push(target);
+					break;
+				case '*':
+				case '/':
+					stack.push(target);
+					break;
+				case '+':
+				case '-':
+					while(!(stack.isEmpty()) && (stack.peek() != '+' || stack.peek() != '-' || stack.peek() != '(')){
+						char tempTarget = stack.pop();
+						if(tempTarget != '('){
+							sb.append(tempTarget);
+						}
+					}
+					stack.push(target);
+					break;
+				case ')':
+					while(!(stack.isEmpty())){
+						char tempTarget = stack.pop();
+						if(tempTarget == '(')
+							break;
+						sb.append(tempTarget);
+					}
+					break;
+				default:
+					sb.append(target);
+					break;
+			}
+		}
+		while(!(stack.isEmpty())){
+			sb.append(stack.pop());
+		}
+		System.out.println(sb.toString());
+		sc.close();
+		
+	}
+}
