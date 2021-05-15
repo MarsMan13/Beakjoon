be50fb79c2bc123402527525a7ee487d643ead7f
MarsMan13 <choigeon0513@gmail.com>
2021-05-15 04:38:54 +0000

    21.05.15.

--- Java_tb/2263.java
+++ Java_tb/2263.java
@@ -4,51 +4,67 @@ import java.io.*;
 
 class Main{
 	
-	static Node root = null;
-	static int in_index = 0;
-	static int post_index = 0;
+	static int n;
+	static int[] inorder = null;
+	static int[] postorder = null;
+	static int[] inorder_index = null;
+	static StringBuilder sb = new StringBuilder();
 	
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(bf.readLine());
-		int[] inorder = new int[n+1];
-		int[] postorder = new int[n+1];
+		n = Integer.parseInt(bf.readLine());
+	
+		inorder = new int[n+1];
+		postorder = new int[n+1];
+		inorder_index = new int[n+1];
+		
 		StringTokenizer st = new StringTokenizer(bf.readLine());
-		for(int i = 1; i<=n; i++)
+		for(int i = 1; i<=n; i++){
 			inorder[i] = Integer.parseInt(st.nextToken());
+			inorder_index[inorder[i]] = i;
+		}
 		st = new StringTokenizer(bf.readLine());
-		for(int i = 1; i<=n; i++)
+		for(int i = 1; i<=n; i++){
 			postorder[i] = Integer.parseInt(st.nextToken());
+		}
 		// END OF INIT
 		
-		root = new Node();
-		def(root);
-		
+		def1(1, n, 1, n);
+		System.out.println(sb.toString());
 	}
 	
-	public static void def(Node node){
-		visit(node, 0);			//pre-order
-		if(node.left != null)
-			def(node.left);
-		visit(node, 1);			//in-order
-		if(node.right != null)
-			def(node.right);
-		visit(node, 2);			//post-order
-		
-	}
+	public static void def1(int s1, int e1, int s2, int e2){
 	
-	public static void visit(Node node, int mode){
-		//mode : 0 --> pre, 1 --> in, 2 --> post
+		int in_s1 = s1, in_e1, in_s2, in_e2 = e1;
 		
-	
+		int root_value = postorder[e2];
+		sb.append(root_value);	sb.append(" ");
+		
+		int mid = inorder_index[root_value];
+		in_e1 = mid-1;
+		if(in_e1 < s1){
+			in_s1 = 0;
+			in_e1 = 0;
+		}
+		in_s2 = mid+1;
+		if(e1 < in_s2){
+			in_s2 = 0;
+			in_e2 = 0;
+		}
+		// END OF INORDER
+		int leftSize = 0;
+		if(in_s1 != 0){
+			leftSize = mid - s1;
+			def1(in_s1, in_e1, s2, s2 + leftSize - 1);
+		}
+		int rightSize = 0;
+		if(in_s2 != 0){
+			rightSize = e1 - mid;
+			def1(in_s2, in_e2, s2 + leftSize, s2 + leftSize + rightSize-1);
+		}
+		
 	}
 }
 
 
-class Node{
-	
-	int index;
-	Node left = null;
-	Node right = null;	
-}
