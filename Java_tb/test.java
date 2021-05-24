fed172aef234bb4c73b38332d2a7bc4e6ef523ab
MarsMan13 <choigeon0513@gmail.com>
2021-05-24 02:40:00 +0000

    21.05.24.

--- Java_tb/test.java
+++ Java_tb/test.java
@@ -6,17 +6,28 @@ class Main{
 
     public static void main(String[] args){
 
+		int[] bag1 = new int[]{7, 9, 11 , 13 , 15};
+		int[] bag2 = new int[]{7, 9, 11 , 13 , 15};
+		int[] bag3 = new int[]{7, 9, 11 , 13 , 15};
 		
-		Deque<Integer> queue = new ArrayDeque<>();
-		queue.offerLast(1);
-		queue.offerLast(3);
-		queue.offerLast(2);
-		queue.offerLast(4);
-		
-		System.out.println(queue.pollFirst());
-		System.out.println(queue.pollFirst());
-		System.out.println(queue.pollFirst());
-		System.out.println(queue.pollFirst());
+		System.out.println(binarySearch(bag1, 0, bag1.length-1, 10));
+	}
+	
+	static int binarySearch(int[] ary, int left, int right, int size){
+	
+		if(size <= ary[left])
+			return left;
+		if(ary[right] < size)
+			return -1;
+		//
+		int mid = (left + right) / 2;
+		if(ary[mid] < size){
+			return binarySearch(ary, mid+1, right, size);
+		}
+		else if(ary[mid] > size){
+			return binarySearch(ary, left, mid, size);
+		}
+		return mid;
 	}
 }
 
