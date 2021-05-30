f3e80420175642242bc84589c84a850708d8247d
MarsMan13 <choigeon0513@gmail.com>
2021-05-30 10:11:07 +0000

    21.05.30.

--- Java_tb/9328.java
+++ Java_tb/9328.java
@@ -22,9 +22,9 @@ class Main{
 			
 			char[][] map = new char[h+2][w+2];
 			for(int i = 0; i<h+2; i++)
-				map[i][0] = map[i][w+1] = '*';
+				map[i][0] = map[i][w+1] = '.';
 			for(int j = 0; j<w+2; j++)
-				map[0][j] = map[h+1][j] = '*';
+				map[0][j] = map[h+1][j] = '.';
 			
 			for(int i = 1; i<=h; i++){
 				String line = bf.readLine();
@@ -47,63 +47,7 @@ class Main{
 			for(int i = 0; i<26; i++)
 				doorQ[i] = new ArrayList<Point>();
 			
-			for(int i = 1; i<=h; i++){
-				if('A' <= map[i][1] && map[i][1] <= 'Z'){
-					if(keyQ[map[i][1]-'A'] != 0){
-						map[i][1] = '.';
-					}
-				}
-				if('A' <= map[i][w] && map[i][w] <= 'Z'){
-					if(keyQ[map[i][w]-'A'] != 0){
-						map[i][w] = '.';
-					}
-				}
-				if(map[i][1] == '.' || map[i][1] == '$'){
-					if(map[i][1] == '$'){
-						ret++;
-						map[i][1] = '.';
-					}
-					queue.add(new Point(i, 1));
-					visited[i][1] = 1;
-				}
-				if(map[i][w] == '.' || map[i][w] == '$'){
-					if(map[i][w] == '$'){
-						ret++;
-						map[i][w] = '.';
-					}
-					queue.add(new Point(i, w));
-					visited[i][w] = 1;
-				}
-			}
-			for(int j = 1; j<=w; j++){
-				if('A' <= map[1][j] && map[1][j] <= 'Z'){
-					if(keyQ[map[1][j]-'A'] != 0){
-						map[1][j] = '.';
-					}
-				}
-				if('A' <= map[h][j] && map[h][j] <= 'Z'){
-					if(keyQ[map[h][j]-'A'] != 0){
-						map[h][j] = '.';
-					}
-				}
-				if(map[1][j] == '.' || map[1][j] == '$'){
-					if(map[1][j] == '$'){
-						ret++;
-						map[1][j] = '.';
-					}
-					queue.add(new Point(1, j));
-					visited[1][j] = 1;
-				}
-				if(map[h][j] == '.' || map[h][j] == '$'){
-					if(map[h][j] == '$'){
-						ret++;
-						map[h][j] = '.';
-					}
-					queue.add(new Point(h, j));
-					visited[h][j] = 1;
-				}
-			}
-		
+			queue.offer(new Point(0, 0));	
 			//
 			while(!(queue.isEmpty())){
 				Point cur = queue.poll();
@@ -111,6 +55,9 @@ class Main{
 					int new_i = cur.i + ii[k];
 					int new_j = cur.j + jj[k];
 					//
+					if(new_i < 0 || h+1 < new_i || new_j < 0 || w+1 < new_j)
+						continue;
+					
 					if(map[new_i][new_j] == '.' && visited[new_i][new_j] == 0){
 						visited[new_i][new_j] = 1;
 						queue.offer(new Point(new_i, new_j));
@@ -135,6 +82,9 @@ class Main{
 							visited[door_i][door_j] = 1;
 							queue.offer(new Point(door_i, door_j));
 						}
+						map[new_i][new_j] = '.';
+						visited[new_i][new_j] = 1;
+						queue.offer(new Point(new_i, new_j));
 					}
 					else if(map[new_i][new_j] == '$'){
 						ret++;
