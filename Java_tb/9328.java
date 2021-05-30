e52e8d5f0f0a3818069820a98a2b0e61ff84faed
MarsMan13 <choigeon0513@gmail.com>
2021-05-30 09:53:09 +0000

    21.05.30. : The half of 9328

--- Java_tb/9328.java
+++ Java_tb/9328.java
@@ -12,7 +12,8 @@ class Main{
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(bf.readLine());
 		int T = Integer.parseInt(st.nextToken());
-		
+	
+		StringBuilder sb = new StringBuilder();
 		for(int t = 0; t<T; t++){
 		
 			st = new StringTokenizer(bf.readLine());
@@ -20,69 +21,90 @@ class Main{
 			int[][] visited = new int[h+2][w+2];
 			
 			char[][] map = new char[h+2][w+2];
-			for(int i = 0; i<w+2; i++)
+			for(int i = 0; i<h+2; i++)
 				map[i][0] = map[i][w+1] = '*';
-			for(int j = 0; j<h+2; j++)
+			for(int j = 0; j<w+2; j++)
 				map[0][j] = map[h+1][j] = '*';
-			for(int i = 0; i<h; i++){
+			
+			for(int i = 1; i<=h; i++){
 				String line = bf.readLine();
-				for(int j = 0; j<w; j++){
-					map[i][j] = line.charAt(j);
+				for(int j = 1; j<=w; j++){
+					map[i][j] = line.charAt(j-1);
 				}
 			}
 			//
 			int[] keyQ = new int[26];
 			String keys = bf.readLine();
-			for(int i = 0; i<keys.length(); i++)
+			for(int i = 0; i<keys.length(); i++){
+				if(keys.charAt(i) == '0')
+					break;
 				keyQ[keys.charAt(i) - 'a'] = 1;
+			}
 			//
+			int ret = 0;
 			Queue<Point> queue = new LinkedList<>();
+			ArrayList[] doorQ = new ArrayList[26];
+			for(int i = 0; i<26; i++)
+				doorQ[i] = new ArrayList<Point>();
+			
 			for(int i = 1; i<=h; i++){
 				if('A' <= map[i][1] && map[i][1] <= 'Z'){
-					if(keyQ[map[i][1]-'A'+'a'] == 1){
+					if(keyQ[map[i][1]-'A'] != 0){
 						map[i][1] = '.';
 					}
 				}
 				if('A' <= map[i][w] && map[i][w] <= 'Z'){
-					if(keyQ[map[i][w]-'A'+'a'] == 1){
+					if(keyQ[map[i][w]-'A'] != 0){
 						map[i][w] = '.';
 					}
 				}
-				if(map[i][1] == '.'){
+				if(map[i][1] == '.' || map[i][1] == '$'){
+					if(map[i][1] == '$'){
+						ret++;
+						map[i][1] = '.';
+					}
 					queue.add(new Point(i, 1));
 					visited[i][1] = 1;
 				}
-				if(map[i][w] == '.'){
+				if(map[i][w] == '.' || map[i][w] == '$'){
+					if(map[i][w] == '$'){
+						ret++;
+						map[i][w] = '.';
+					}
 					queue.add(new Point(i, w));
 					visited[i][w] = 1;
 				}
 			}
 			for(int j = 1; j<=w; j++){
 				if('A' <= map[1][j] && map[1][j] <= 'Z'){
-					if(keyQ[map[1][j]-'A'+'a'] == 1){
+					if(keyQ[map[1][j]-'A'] != 0){
 						map[1][j] = '.';
 					}
 				}
 				if('A' <= map[h][j] && map[h][j] <= 'Z'){
-					if(keyQ[map[h][j]-'A'] == 1){
+					if(keyQ[map[h][j]-'A'] != 0){
 						map[h][j] = '.';
 					}
 				}
-				if(map[1][j] == '.'){
+				if(map[1][j] == '.' || map[1][j] == '$'){
+					if(map[1][j] == '$'){
+						ret++;
+						map[1][j] = '.';
+					}
 					queue.add(new Point(1, j));
 					visited[1][j] = 1;
 				}
-				if(map[w][j] == '.'){
-					queue.add(new Point(w, j));
-					visited[w][j] = 1;
+				if(map[h][j] == '.' || map[h][j] == '$'){
+					if(map[h][j] == '$'){
+						ret++;
+						map[h][j] = '.';
+					}
+					queue.add(new Point(h, j));
+					visited[h][j] = 1;
 				}
 			}
 		
 			//
-			ArrayList[] doorQ = new ArrayList[26];
-			for(int i = 0; i<26; i++)
-				doorQ[i] = new ArrayList<Point>();
-			int ret = 0;
 			while(!(queue.isEmpty())){
 				Point cur = queue.poll();
 				for(int k = 0; k<4; k++){
@@ -104,6 +126,16 @@ class Main{
 							doorQ[doorIndex].add(new Point(new_i, new_j));
 						}
 					}
+					else if('a' <= map[new_i][new_j] && map[new_i][new_j] <= 'z'){
+						int keyIndex = map[new_i][new_j] - 'a';
+						keyQ[keyIndex]++;
+						for(Point p : (ArrayList<Point>)doorQ[keyIndex]){
+							int door_i = p.i;	int door_j = p.j;
+							map[door_i][door_j] = '.';
+							visited[door_i][door_j] = 1;
+							queue.offer(new Point(door_i, door_j));
+						}
+					}
 					else if(map[new_i][new_j] == '$'){
 						ret++;
 						map[new_i][new_j] = '.';
@@ -113,9 +145,9 @@ class Main{
 				}
 			}
 			
-			
-			
+			sb.append(ret);	sb.append("\n");	
 		}
+		System.out.println(sb.toString());
 	}
 	
 	static int[] ii = {1, -1, 0, 0};
