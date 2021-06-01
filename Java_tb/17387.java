22c56eb238b1db89217e974fb5994a4986df964d
MarsMan13 <choigeon0513@gmail.com>
2021-06-01 10:59:36 +0000

    21.06.01.

--- Java_tb/17387.java
+++ Java_tb/17387.java
@@ -23,19 +23,44 @@ class Main{
 		long ret2 = (1L*(x2-x1)*y4-(1L*(y2-y1)*(x4-x1)+1L*y1*(x2-x1)));
 		long ret3 = (1L*(x4-x3)*y1-(1L*(y4-y3)*(x1-x3)+1L*y3*(x4-x3)));
 		long ret4 = (1L*(x4-x3)*y2-(1L*(y4-y3)*(x2-x3)+1L*y3*(x4-x3)));
-		
-		if(ret1*ret2 <= 0 && ret3*ret4 <= 0){
-			if(ret1 == 0 && ret2 == 0){
-				if(((x3 < x1) && (x4 < x1) && (x3 < x2) && (x4 < x2)) || ((x3 > x1) && (x4 > x1) && (x3 > x2) && (x4 > x2)))
-					System.out.println(0);
-				else if(((y3 < y1) && (y4 < y1) && (y3 < y2) && (y4 < y2)) || ((y3 > y1) && (y4 > y1) && (y3 > y2) && (y4 > y2)))
+	
+		if(ret1*ret2 == 0 && ret3*ret4 == 0){
+			// about x1, y1 : a / x2, y2 : b
+			if(y2 < y1 || (y2 == y1 && x2 < x1)){
+				{
+					long temp = y2;	y2 = y1;	y1 = temp;
+				}
+				{
+					long temp = x2;	x2 = x1;	x1 = temp;
+				}
+			}
+			if(y4 < y3 || (y4 == y3 && x4 < x3)){
+				{
+					long temp = y4;	y4 = y3;	y3 = temp;
+				}
+				{
+					long temp = x4;	x4 = x3;	x3 = temp;
+				}
+			}
+			//
+			if(((x1 == x3 && y1 == y3) || (x2 == x4 && y2 == y4)) || (x1 == x4 && y1 == y4) || (x2 == x3 && y2 == y3)){
+				System.out.println(1);
+			}
+			else{
+				int flag1 = 0;	int flag2 = 0;
+				if(y1 < y4 || (y1 == y4 && x1 < x4))
+					flag1 = 1;
+				if(y2 < y3 || (y2 == y3 && x2 < x3))
+					flag2 = 1;
+				if(flag1 == flag2)
 					System.out.println(0);
 				else
 					System.out.println(1);
-			}
-			else
-				System.out.println(1);
-		}	
+			}	
+			
+		}
+		else if(ret1*ret2 <= 0 && ret3*ret4 <= 0)
+			System.out.println(1);
 		else
 			System.out.println(0);
 		
