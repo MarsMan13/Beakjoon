1956d2c60043cafd4951f9c54d2532e7344ff873
MarsMan13 <choigeon0513@gmail.com>
2021-06-07 09:25:59 +0000

    21.06.07.

--- Java_tb/test.java
+++ Java_tb/test.java
@@ -6,33 +6,9 @@ class Main{
 	
 	
 	public static void main(String[] args) throws IOException {
-		
-		Pair p1 = new Pair(0, 1);
-		Pair p2 = new Pair(1, -1);
-		
-		System.out.println(Pair.angle(p1, p2));
-	}
+	
+		int A = 16;
+		System.out.println(A & (-1 * A));
+	}		
 }
 
-class Pair{
-	int i, j;
-	
-	Pair(int i, int j){
-		this.i = i;	this.j = j;
-	}
-	
-	
-	static double distance(Pair p1, Pair p2){
-		int xd2 = (int)Math.pow((p1.i - p2.i), 2);
-		int yd2 = (int)Math.pow((p1.j - p2.j), 2);
-		return Math.sqrt(xd2+yd2);
-	}
-	
-	public double distanceFromZero(){
-		return distance(this, new Pair(0, 0));
-	}
-	
-	static double angle(Pair u1, Pair u2){
-		return Math.acos(((u1.i * u2.i) + (u1.j * u2.j))/(u1.distanceFromZero() * u2.distanceFromZero()));
-	}
-}
