51103081713742a48c574b8885862ca7120ba811
MarsMan13 <choigeon0513@gmail.com>
2021-06-11 02:16:00 +0000

    21.06.11.

---
+++ Java_tb/15486.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.io.*;
+
+
+class Main{
+	
+	static int N;
+	static LinkedList[] cc = null;
+	static int[] dp = null;
+	
+	public static void main(String[] args) throws IOException {
+		
+		
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		N = Integer.parseInt(st.nextToken());
+		cc = new LinkedList[N+1];	dp = new int[N+1];
+		for(int i = 1; i<=N; i++)	cc[i] = new LinkedList<Counseling>();
+		//
+		
+		for(int i = 1; i<=N; i++){
+			st = new StringTokenizer(bf.readLine());
+			int tempT = Integer.parseInt(st.nextToken());
+			int tempP = Integer.parseInt(st.nextToken());
+			//
+			if(i + tempT - 1 <=N)
+				cc[i+tempT-1].add(new Counseling(tempT, tempP));
+		}
+		//
+		
+		for(int i = 1; i<=N; i++){
+			int tempMax = dp[i-1];
+			for(Iterator<Counseling> itr = cc[i].iterator(); itr.hasNext();){
+				Counseling c = itr.next();
+				int tempSum = dp[i - c.T] + c.P;
+				if(tempMax < tempSum)
+					tempMax = tempSum;
+			}
+			dp[i] = tempMax;
+		}
+		System.out.println(dp[N]);
+	}
+}
+
+class Counseling{
+	int T, P;
+	Counseling(int T, int P){
+		this.T = T;	this.P = P;
+	}
+	
+	@Override
+	public String toString(){
+		return "T: "+T+", P: "+P;
+	}
+}
--- Java_tb/1562.java
+++ Java_tb/1562.java
@@ -3,13 +3,48 @@ import java.util.*;
 
 class Main{
 	
+	static int[][] dp = null;
+	static final int INF = 1000000000;
 	
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		
+		dp = new int[10][N+1];
 		
+		for(int i = 1; i<10; i++)	dp[i][1] = 1;
+		//
+		for(int j = 2; j<=N; j++){
+			for(int i = 0; i<10; i++){
+				int tempSum = 0;
+				if(0 <= i-1)
+					tempSum = (tempSum + dp[i-1][j-1]) % INF;
+				if(i+1 < 10)
+					tempSum = (tempSum + dp[i+1][j-1]) % INF;
+				//
+				dp[i][j] = tempSum;	
+			}
+		}
+		int sum = 0;
+		for(int i = 1; i<10; i++)
+			sum = (sum + dp[i][N])%INF;
+		// PROCESS ABOUT ALL
+		
+		for(int j = 2; j<=N; j++){
+			for(int i = 0; i<10; i++){
+				int tempSum = 0;
+				if(0 <= i-1)
+					tempSum = (tempSum + dp[i-1][j-1]) % INF;
+				if(i+1 < 10)
+					tempSum = (tempSum + dp[i+1][j-1]) % INF;
+				//
+				dp[i][j] = tempSum;	
+			}
+		}
+		int sum = 0;
+		for(int i = 1; i<10; i++)
+			sum = (sum + dp[i][N])%INF;
 		
 	}
 }
