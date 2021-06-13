27cd296667f5424f484a6c8a4274d3402b85899a
MarsMan13 <choigeon0513@gmail.com>
2021-06-13 10:00:18 +0000

    21.06.13.

---
+++ Java_tb/14226.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+
+class Main{
+	
+	static int[] dp = null;
+	static int S;
+	static final int MAX = 1000000;
+	
+	public static void main(String[] args) {
+		
+		
+		Scanner sc = new Scanner(System.in);
+		S = sc.nextInt();	sc.close();
+		dp = new int[2*S+1];	Arrays.fill(dp, 1000000);
+		dp[1] = 0;
+		
+		for(int i = 1; i<=2*S; i++){
+			// copy my self
+			int paste = 1;	int copy = 1;
+			for(int j = 2*i; j<=2*S; j+=i){
+				if(dp[i] + copy + paste < dp[j]){
+					dp[j] = dp[i] + copy + paste;
+				}
+				paste++;
+			}
+			// minor from i
+			int minor = 1;
+			for(int j = i-1; i/2<j; j--){
+				if(dp[i] + minor < dp[j]){
+					dp[j] = dp[i] + minor;
+				}
+				minor++;
+			}
+		}
+		
+		System.out.println(dp[S]);
+	}
+	
+}
--- Java_tb/2631.java
+++ Java_tb/2631.java
@@ -4,12 +4,19 @@ import java.io.*;
 
 class Main{
 	
+	static int N;
+	static int[] input = null;
 	
 	public static void main(String[] args) throws IOException {
 		
 		
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(bf.readLine());
+		N = Integer.parseInt(st.nextToken());
+		
+		
+		
+		
 		
 	}
 }
