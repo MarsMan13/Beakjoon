b69b78bf6f22df78ed55de08950aff110e4df57d
MarsMan13 <choigeon0513@gmail.com>
2021-06-18 07:14:08 +0000

    21.06.18.

--- Java_tb/9084.java
+++ Java_tb/9084.java
@@ -11,33 +11,37 @@ class Main{
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(bf.readLine());
 		
+		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(st.nextToken());
 		for(int t = 0; t<T; t++){
+			int maxValue = -1;
+			// input1 : The number of coins
 			st = new StringTokenizer(bf.readLine());
 			int N = Integer.parseInt(st.nextToken());
+			// input2 : usable coins
 			st = new StringTokenizer(bf.readLine());
-			int[] coins = new int[N];
-			for(int i = 0; i<N; i++)
+			int[] coins = new int[N+1];
+			for(int i = 1; i<=N; i++){
 				coins[i] = Integer.parseInt(st.nextToken());
+				if(maxValue < coins[i])	maxValue = coins[i];
+			}
+			// input3 : target
 			st = new StringTokenizer(bf.readLine());
 			int target = Integer.parseInt(st.nextToken());
+			if(maxValue < target)	maxValue = target;
 			// END OF INIT
 			
-			int[] result = new int[target+1];
-			
-			for(int i = 0; i<N; i++){
-				if(coins[i] <= target)	
-					result[coins[i]] = 1;
-			}
-			
-			for(int i = 1; i<=target; i++){
-				for(int j = 1; j<=i; j++){
-					result[i] += (result[j] * result[i-j]);
-					if(result[i] != 0)
-						System.out.println(i+" : "+result[i]);
+			int[][] dp = new int[N+1][maxValue+1];
+			for(int i = 1; i<=N; i++)	dp[i][coins[i]] = 1;
+			for(int i = 1; i<=N; i++){
+				for(int j = 1; j<=target; j++){
+					dp[i][j] += dp[i - 1][j];
+					if(0 <= j - coins[i])
+						dp[i][j] += dp[i][j - coins[i]];
 				}
 			}
-			System.out.println(result[target]);
+			sb.append(dp[N][target]);	sb.append("\n");
 		}
+		System.out.print(sb.toString());
 	}
 }
