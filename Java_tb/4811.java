055533bb769d13da31dffcafd9ee287a22d69dd2
MarsMan13 <choigeon0513@gmail.com>
2021-06-19 01:57:13 +0000

    21.06.19.

---
+++ Java_tb/4811.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+
+class Main{
+	
+	
+	public static void main(String[] args) throws IOException {
+		
+		
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		int T = Integer.parseInt(st.nextToken());
+		for(int t = 0; t<T; t++){
+			st = new StringTokenizer(bf.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			//
+		}
+	}
+}
