8a0ce0a43d7c7d203ab44b66aea91f3008f8d38e
MarsMan13 <choigeon0513@gmail.com>
2021-06-19 11:10:31 +0000

    21.06.19.

--- Java_tb/4811.java
+++ Java_tb/4811.java
@@ -9,12 +9,29 @@ class Main{
 		
 		
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(bf.readLine());
-		int T = Integer.parseInt(st.nextToken());
-		for(int t = 0; t<T; t++){
+		StringTokenizer st = null;
+		StringBuilder sb = new StringBuilder();
+		while(true){
 			st = new StringTokenizer(bf.readLine());
 			int N = Integer.parseInt(st.nextToken());
+			if(N == 0)	break;
 			//
+			int maxW = N;
+			int maxH = 2*N;
+			long[][] dp = new long[maxW+1][maxH+1];	dp[0][1] = 1;
+			for(int i = 0; i<=maxW; i++){
+				for(int j = 0; j<=maxH; j++){
+					if(i == 0 && j == 1)	continue;
+					if(0 <= i-1 && j+1 <= maxH)
+						dp[i][j] += (0L + dp[i-1][j+1]);
+					if(0 <= j-1)
+						dp[i][j] += (0L + dp[i][j-1]);
+				}
+			}
+			sb.append(dp[N][0]);	sb.append("\n");
 		}
+		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write(sb.toString());	bw.flush();	bw.close();
 	}
 }
