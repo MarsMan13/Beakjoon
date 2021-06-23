744d273d107112c83b1ce198408b5d28db82e5d8
MarsMan13 <choigeon0513@gmail.com>
2021-06-23 11:14:39 +0000

    21.06.23.

--- Java_tb/1562.java
+++ Java_tb/1562.java
@@ -3,48 +3,49 @@ import java.util.*;
 
 class Main{
 	
-	static int[][] dp = null;
-	static final int INF = 1000000000;
+	static final int DIV = 1000000000;
+	static int[][][] dp = null;
 	
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
+		int N = sc.nextInt();	sc.close();
 		
-		dp = new int[10][N+1];
+		dp = new int[N+1][10][1500];
+		for(int i = 1; i<=N; i++)
+			for(int j = 0; j<10; j++)
+				for(int k = 0; k<1500; k++)
+					dp[i][j][k] = -1;
 		
-		for(int i = 1; i<10; i++)	dp[i][1] = 1;
-		//
-		for(int j = 2; j<=N; j++){
-			for(int i = 0; i<10; i++){
-				int tempSum = 0;
-				if(0 <= i-1)
-					tempSum = (tempSum + dp[i-1][j-1]) % INF;
-				if(i+1 < 10)
-					tempSum = (tempSum + dp[i+1][j-1]) % INF;
-				//
-				dp[i][j] = tempSum;	
+		for(int i = 0; i<10; i++)
+			CHECKER = CHECKER | (1 << i);
+		
+		long sum = 0;
+		for(int i = 0; i<10; i++){
+			sum = (sum + 1L * dfs(N, i, (1<<i)))%DIV;
+		}
+		System.out.println(sum);
+	}
+	
+	static int CHECKER = 0;
+	
+	public static int dfs(int index, int i, int before){
+		if(index < 1)
+			return 0;
+		if((before & CHECKER) == CHECKER){
+			return 1;
+		}
+		
+		if(dp[index][i][before] != -1)
+			return dp[index][i][before];
+		
+		int sum = 0;
+		for(int ii = -1; ii<=1; ii+=2){
+			if(0 <= i+ii && i+ii < 10){
+				if(i+ii == 0 && index-1 == 1)	continue;
+				sum = (sum + dfs(index-1, i+ii, (before | (1 << i+ii)))) % DIV;
 			}
 		}
-		int sum = 0;
-		for(int i = 1; i<10; i++)
-			sum = (sum + dp[i][N])%INF;
-		// PROCESS ABOUT ALL
-		
-		for(int j = 2; j<=N; j++){
-			for(int i = 0; i<10; i++){
-				int tempSum = 0;
-				if(0 <= i-1)
-					tempSum = (tempSum + dp[i-1][j-1]) % INF;
-				if(i+1 < 10)
-					tempSum = (tempSum + dp[i+1][j-1]) % INF;
-				//
-				dp[i][j] = tempSum;	
-			}
-		}
-		int sum = 0;
-		for(int i = 1; i<10; i++)
-			sum = (sum + dp[i][N])%INF;
-		
+		return dp[index][i][before] = sum;
 	}
 }
