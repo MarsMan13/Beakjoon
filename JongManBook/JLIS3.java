a6731b236413990dbf1c1b90c7f025a8c8e4c3e1
MarsMan13 <choigeon0513@gmail.com>
2021-07-14 14:24:27 +0000

    21.07.14.

--- JongManBook/JLIS3.java
+++ JongManBook/JLIS3.java
@@ -7,6 +7,7 @@ class Main{
 	static int n = 0, m = 0;
 	static int[] input1 = new int[1];
 	static int[] input2 = new int[1];
+	
 	static int[][] dp = new int[1][1];
 	
 	public static void main(String[] args) throws IOException {
@@ -24,15 +25,58 @@ class Main{
 			st = new StringTokenizer(bf.readLine());
 			for(int j = 1; j<=m; j++)	input2[j] = Integer.parseInt(st.nextToken());
 			//
-			dp = new int[n+1][m+1];
-			
+			//dp = new int[n+1][m+1];
+			int result = 0;
+			for(int tt = 0; tt<=1; tt++){
+				dp = new int[n+1][m+1];
+				for(int i = 1; i<=n; i++){
+					result = Math.max(result, def(i-1, m, input1[i]));
+				}
+				
+				// END OF FIRST RUN;
+				{
+					int temp = n;
+					n = m;
+					m = temp;
+					//
+					int[] tempInput = input1;
+					input1 = input2;
+					input2 =tempInput;
+				}
+			}
+			// dp = new int[n+1][m+1];
+			// for(int i = 1; i<=n; i++){
+			// 	result = Math.max(result, def(i-1, m, input1[i]));
+			// }
+			sb.append(result);	sb.append("\n");
 		}
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		bw.write(sb.toString());	bw.flush(); bw.close();
 	}
 	
-	static int def(int i, int j){
+	public static int def(int i, int j, int min){
+		if(i<0 || j<0)	return 0;
+		// base case
 		
+		if(dp[i][j] != 0){
+			return dp[i][j];
+		}
+		
+		int ret = 0;
+		{
+			for(int ii = i; 0<=ii; ii--){
+				if(input1[ii] < min){
+					ret = Math.max(ret, def(ii-1, j, input1[ii]));
+				}
+				for(int jj = j; 0<=jj; jj--){
+					if(input2[jj] < min){
+						ret = Math.max(ret, def(ii, jj-1, input2[jj]));
+					}
+				}
+			}
+		}
+		// System.out.printf("i: %d, j: %d == %d\n", i, j, ret+1);
+		return dp[i][j] = ret + 1;
 	}
 }
 
