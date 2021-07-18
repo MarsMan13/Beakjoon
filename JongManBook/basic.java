065ebe78b5f112652e6c93b3fbe53633e4ba4c3c
MarsMan13 <choigeon0513@gmail.com>
2021-07-18 06:54:05 +0000

    21.07.18.

--- JongManBook/basic.java
+++ JongManBook/basic.java
@@ -9,7 +9,14 @@ class Main{
 		
 		
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(bf.readLine());
-		
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(bf.readLine());
+		for(int t = 0; t<T; t++){
+			StringTokenizer st = new StringTokenizer(bf.readLine());
+	
+				
+		}
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write(sb.toString());	bw.flush();	bw.close();
 	}
 }
